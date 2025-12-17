82410b0e9213aef18678f2690a93c179381e6625
Bagas0106 <rbagas608@gmail.com>
2025-12-17 19:34:11 +0700

    perubahan search

--- main.cpp
+++ main.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 
 int main(){
-    cout << "|==========================================================|" << endl;
-    cout << "|                           MENU                           |"<< endl;
-    cout << "|==========================================================|"<< endl;
-    cout << "|1.                                                        |"<< endl;
-    cout << "|2.                                                        |"<< endl;
-    cout << "|3.                                                        |"<< endl;
-    cout << "|4.                                                        |"<< endl;
-    cout << "|5.                                                        |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|                                                          |"<< endl;
-    cout << "|__________________________________________________________| " << endl;
+    cout << "==========================================================" << endl;
+    cout << "                           MENU                           "<< endl;
+    cout << "=========================================================="<< endl;
+    cout << "1.                                                        "<< endl;
+    cout << "2.                                                        "<< endl;
+    cout << "3.                                                        "<< endl;
+    cout << "4.                                                        "<< endl;
+    cout << "5.                                                        "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "                                                          "<< endl;
+    cout << "==========================================================" << endl;
     return 0;
 }
